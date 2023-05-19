7c800dcd1989d74483ccde84c609783b48e80c9b
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-05-19 18:28:03 +0600

    get operation for toy details

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 require('dotenv').config()
 
@@ -26,12 +26,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const productCollection=client.db('kitsForKids').collection('products');
+
+    app.get('/products',async(req,res)=>{
+      const cursor=productCollection.find();
+      const result=await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/products/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id : new ObjectId(id)}
+      const result=await productCollection.findOne(query)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
